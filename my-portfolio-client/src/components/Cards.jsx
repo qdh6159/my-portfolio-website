2837ed361b013b70d307897b6919140d31843bf1
Quincy Hicks <qdh6159@gmail.com>
2019-11-24 14:05:40 -0600

    added image

--- my-portfolio-client/src/components/Cards.jsx
+++ my-portfolio-client/src/components/Cards.jsx
@@ -11,9 +11,9 @@ const Cards = (props) => {
           <Card >
         <CardImg width="100%" src="https://cdn.shopify.com/s/files/1/1721/2115/products/RickandMorty_1024x1024.jpg?v=1539628583" alt="Card image cap" />
         <CardImgOverlay>
-          <h1>Card Title</h1>
+          <p id="titles">Rick's World API</p>
           
-          <Button theme="secondary">Go</Button>
+          <Button theme="primary" size="lg"  block squared><span class="view">View</span></Button>
 
         </CardImgOverlay>
       </Card>
@@ -21,13 +21,11 @@ const Cards = (props) => {
 
           <li>
           <Card >
-        <CardImg width="100%" src="https://cdn.shopify.com/s/files/1/1721/2115/products/RickandMorty_1024x1024.jpg?v=1539628583" alt="Card image cap" />
+        <CardImg width="100%" src="" alt="Card image cap" />
         <CardImgOverlay>
-          <CardTitle>Card Title</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </CardText>
+        <p id="titles">Resume</p>
+          
+          <Button theme="primary" size="lg"  block squared><span class="view">View</span></Button>
         </CardImgOverlay>
       </Card>
           </li>
