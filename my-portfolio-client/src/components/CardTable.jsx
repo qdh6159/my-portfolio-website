5be623b1ca6860c646ed668670d21d113b31d4ca
Quincy Hicks <qdh6159@gmail.com>
2019-11-24 08:57:38 -0600

    added card table

---
+++ my-portfolio-client/src/components/CardTable.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+
+function CardTable() {
+    return (
+      <div class="cardTable">
+        <h1> Main Box</h1>
+      </div>
+    );
+  }
+  
+  export default CardTable;
--- my-portfolio-client/src/components/MainBox.jsx
+++ my-portfolio-client/src/components/MainBox.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import CardTable from './CardTable'
 
 
 function MainBox() {
     return (
       <div class="mainBox">
-        <h1> Main Box</h1>
+        {/* <h1> Main Box</h1> */}
+        <CardTable />
       </div>
     );
   }
