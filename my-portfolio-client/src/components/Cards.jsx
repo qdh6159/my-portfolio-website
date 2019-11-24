cfc2d7252ccbef8c2f10def9b44a168a89ffb417
Quincy Hicks <qdh6159@gmail.com>
2019-11-24 15:45:32 -0600

    update before deployment to AWS

--- my-portfolio-client/src/components/Cards.jsx
+++ my-portfolio-client/src/components/Cards.jsx
@@ -21,7 +21,7 @@ const Cards = (props) => {
 
           <li>
           <Card >
-        <CardImg width="100%" src="" alt="Card image cap" />
+        <CardImg width="100%" src="my-portfolio-website/images/UNADJUSTEDNONRAW_thumb_23e8.jpg" alt="Card image cap" />
         <CardImgOverlay>
         <p id="titles">Resume</p>
           
@@ -32,13 +32,23 @@ const Cards = (props) => {
 
           <li>
           <Card >
-        <CardImg width="100%" src="https://cdn.shopify.com/s/files/1/1721/2115/products/RickandMorty_1024x1024.jpg?v=1539628583" alt="Card image cap" />
+        <CardImg width="100%" src="" alt="Card image cap" />
         <CardImgOverlay>
-          <CardTitle>Card Title</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </CardText>
+        <p id="titles">ClientVault App</p>
+          
+          <Button theme="primary" size="lg"  block squared><span class="view">View</span></Button>
+        </CardImgOverlay>
+      </Card>
+          </li>
+
+
+          <li>
+          <Card >
+        <CardImg width="100%" src="" alt="Card image cap" />
+        <CardImgOverlay>
+        <p id="titles">About</p>
+          
+          <Button theme="primary" size="lg"  block squared><span class="view">View</span></Button>
         </CardImgOverlay>
       </Card>
           </li>
