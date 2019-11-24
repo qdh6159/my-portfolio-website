1b9abfa2588fc1cff226fb2c6889988776caea48
Quincy Hicks <qdh6159@gmail.com>
2019-11-24 08:47:00 -0600

    set up main box

--- my-portfolio-client/src/App.js
+++ my-portfolio-client/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
+import MainBox from './components/MainBox'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+     
+      <p>
+          Welcome to quincyhicks.com
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <MainBox />
     </div>
   );
 }
---
+++ my-portfolio-client/src/components/MainBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+
+function MainBox() {
+    return (
+      <div class="mainBox">
+        <h1> Main Box</h1>
+      </div>
+    );
+  }
+  
+  export default MainBox;
